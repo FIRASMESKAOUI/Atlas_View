a2169914e338ba4724afd89dbe02a31724293f5d
firas <firasmeskaoui2@gmail.com>
2025-11-09 00:42:12 +0100

    update index.html

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import sys
 # DON'T CHANGE THIS !!!
 sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
 
-from flask import Flask, send_from_directory, request, jsonify
+from flask import Flask, send_from_directory, request, jsonify, render_template_string
 from flask_cors import CORS
 from dotenv import load_dotenv
 # from .database.mongodb import init_mongodb
@@ -40,6 +40,9 @@ def after_request(response):
     response.headers.add('Access-Control-Allow-Origin', '*')
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    # Ajouter les headers de cache pour les métadonnées Open Graph
+    response.headers.add('Cache-Control', 'public, max-age=3600')
+    response.headers.add('X-Content-Type-Options', 'nosniff')
     return response
 
 @app.route('/api/bvmt/market', methods=['GET'])
@@ -77,7 +80,16 @@ def serve(path):
     else:
         index_path = os.path.join(static_folder_path, 'index.html')
         if os.path.exists(index_path):
-            return send_from_directory(static_folder_path, 'index.html')
+            with open(index_path, 'r', encoding='utf-8') as f:
+                html_content = f.read()
+
+            # Améliorer les en-têtes pour le partage social
+            response = app.make_response(html_content)
+            response.headers['Content-Type'] = 'text/html; charset=utf-8'
+            response.headers['X-UA-Compatible'] = 'ie=edge'
+            response.headers['Referrer-Policy'] = 'strict-origin-when-cross-origin'
+            response.headers['Permissions-Policy'] = 'geolocation=(), microphone=(), camera=()'
+            return response
         else:
             return "index.html not found", 404
 
